f04c15a2b785adaf5c7de85b7e5027527b73459d
lemuel <lemuelinchrist@gmail.com>
2017-07-29 15:16:15 +0800

    Create Spring Boot structure with Gradle Project

---
+++ src/main/java/com/lemuelinchrist/exercise/facepalm/controllers/AccountController.java
@@ -0,0 +1,8 @@
+package com.lemuelinchrist.exercise.facepalm.controllers;
+
+/**
+ * @author Lemuel Cantos
+ * @since 29/7/2017
+ */
+public class AccountController {
+}
---
+++ src/test/java/com/lemuelinchrist/exercise/facepalm/FacepalmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lemuelinchrist.exercise.facepalm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FacepalmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
