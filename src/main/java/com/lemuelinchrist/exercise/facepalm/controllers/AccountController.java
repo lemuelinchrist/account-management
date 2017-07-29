658b6bdf78f04bbcc2d20417b26e2f79015747e1
lemuel <lemuelinchrist@gmail.com>
2017-07-29 21:18:41 +0800

    add Create and get Account services in the controller

--- src/main/java/com/lemuelinchrist/exercise/facepalm/controllers/AccountController.java
+++ src/main/java/com/lemuelinchrist/exercise/facepalm/controllers/AccountController.java
@@ -1,8 +1,42 @@
 package com.lemuelinchrist.exercise.facepalm.controllers;
 
+import com.lemuelinchrist.exercise.facepalm.model.Account;
+import com.lemuelinchrist.exercise.facepalm.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
 /**
  * @author Lemuel Cantos
  * @since 29/7/2017
  */
+@RestController
+@RequestMapping("/accounts")
 public class AccountController {
+
+    private AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    ResponseEntity<?> create(@RequestBody Account newAccount) {
+        Long id = accountService.save(newAccount);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path(
+                "/{id}").buildAndExpand(id).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    Account get(@PathVariable Long id) {
+        return accountService.findOne(id);
+    }
+
+
+
 }
---
+++ src/test/java/com/lemuelinchrist/exercise/facepalm/AccountControllerTest.java
@@ -0,0 +1,92 @@
+package com.lemuelinchrist.exercise.facepalm;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lemuelinchrist.exercise.facepalm.controllers.AccountController;
+import com.lemuelinchrist.exercise.facepalm.model.Account;
+import com.lemuelinchrist.exercise.facepalm.service.AccountService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.Assert.fail;
+
+/**
+ * @author Lemuel Cantos
+ * @since 29/7/2017
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = AccountController.class, secure = false)
+public class AccountControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    AccountService accountService;
+
+    @Test
+    public void createAccount() throws Exception {
+        Account newAccount = new Account();
+        newAccount.setEmail("testEmail@gmail.com");
+        newAccount.setId(1L);
+        Mockito.when(accountService.save(newAccount)).thenReturn(1L);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/accounts")
+                .accept(MediaType.APPLICATION_JSON).content(convertToJson(newAccount))
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andReturn();
+
+        Assert.assertTrue(result.getResponse().getHeader("Location").contains("/accounts/1"));
+
+    }
+
+    @Test
+    public void retrieveAccount() throws Exception {
+        Account newAccount = new Account();
+        newAccount.setEmail("testEmail@gmail.com");
+        newAccount.setId(1L);
+
+        Mockito.when(accountService.findOne(Mockito.anyLong())).thenReturn(newAccount);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/accounts/1")
+                .accept(MediaType.APPLICATION_JSON).content(convertToJson(newAccount))
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+
+    }
+
+    public String convertToJson(Object o) {
+        ObjectMapper mapper = new ObjectMapper();
+        //Object to JSON in String
+        try {
+            return mapper.writeValueAsString(o);
+
+        } catch (JsonProcessingException e) {
+
+            fail("error during json conversion: " + e.getMessage());
+        }
+        return null;
+    }
+
+
+}
