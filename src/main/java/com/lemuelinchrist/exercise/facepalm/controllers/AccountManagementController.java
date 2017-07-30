3eaefa6a7149ae7b0527700afadeda1ce219623d
lemuel <lemuelinchrist@gmail.com>
2017-07-30 22:35:32 +0800

    [USER STORY 1] add documentation

--- src/main/java/com/lemuelinchrist/exercise/facepalm/controllers/AccountManagementController.java
+++ src/main/java/com/lemuelinchrist/exercise/facepalm/controllers/AccountManagementController.java
@@ -31,6 +31,27 @@ public class AccountManagementController {
         this.accountService = accountService;
     }
 
+    /**
+     * USER STORY # 1
+     * This Service will create a friend connection (befriend) between two email addresses.
+     * The Json Object will have the following structure:
+     * <p>
+     * {
+     * friends:
+     * [
+     * 'andy@example.com',
+     * 'john@example.com'
+     * ]
+     * }
+     * <p>
+     * the JSON response will return a { "success" : true }
+     *
+     * @param beFriendDTO A json object containing two email addresses
+     * @return
+     * @throws InvalidParameterException   will be thrown if the frinds list doesn't exactly have two emails, or if there is an
+     *                                     empty email, or if there is a malformed email
+     * @throws NonExistentAccountException The service will not accept emails that do not have a created account yet
+     */
     @RequestMapping(value = "/befriend", method = RequestMethod.POST)
     public ResponseEntity<Map<String, String>> befriend(@RequestBody BeFriendDTO beFriendDTO) throws InvalidParameterException, NonExistentAccountException {
         beFriendDTO.checkValidity();
