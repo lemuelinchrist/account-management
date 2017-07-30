161b6c7e7475ca9078f355977aa0aab686f9bcb8
lemuel <lemuelinchrist@gmail.com>
2017-07-30 22:47:10 +0800

    [USER STORY 1] add some more docs

--- src/main/java/com/lemuelinchrist/exercise/facepalm/service/AccountService.java
+++ src/main/java/com/lemuelinchrist/exercise/facepalm/service/AccountService.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 /**
  * Account Service is the Service bean that handles all kinds of management of Account from CRUD operations to querying.
+ * This class also contains all logic for the given user stories of the project.
  *
  * @author Lemuel Cantos
  * @since 29/7/2017
@@ -27,12 +28,28 @@ public class AccountService {
     }
 
 
+    /**
+     * Creates a new Account. It contains an email field that should be unique to all accounts.
+     *
+     * @param newAccount This Account object requires a unique email field.
+     * @return returns a generated Id of the object
+     * @throws ExistingEmailException Thrown if the email of the Account object already exists in the database.
+     */
     public Long save(Account newAccount) throws ExistingEmailException {
         if (accountRepository.findByEmail(newAccount.getEmail()).isPresent()) throw new ExistingEmailException();
         accountRepository.save(newAccount);
         return newAccount.getId();
     }
 
+    /**
+     * USER STORY #1
+     * This function will create a friend connection between two accounts of the given emails
+     *
+     * @param firstEmail The email of the first Account
+     * @param secondEmail The email of the second Account
+     * @return returns a list of the two involved accounts with their updated friends field
+     * @throws NonExistentAccountException Thrown if
+     */
     public List<Account> befriendAccounts(String firstEmail, String secondEmail) throws NonExistentAccountException {
         Account firstAccount = accountRepository.findByEmail(firstEmail).orElseThrow(NonExistentAccountException::new);
         Account secondAccount = accountRepository.findByEmail(secondEmail).orElseThrow(NonExistentAccountException::new);
