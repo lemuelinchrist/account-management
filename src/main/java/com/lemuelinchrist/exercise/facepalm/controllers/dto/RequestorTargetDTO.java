dbf10468bc9dc462c8feed82b8626fd5438fa809
lemuel <lemuelinchrist@gmail.com>
2017-07-31 22:30:37 +0800

    [USER STORY 4 and 5] create DTO RequestorTargetDTO

---
+++ src/main/java/com/lemuelinchrist/exercise/facepalm/controllers/dto/RequestorTargetDTO.java
@@ -0,0 +1,26 @@
+package com.lemuelinchrist.exercise.facepalm.controllers.dto;
+
+/**
+ * @author Lemuel Cantos
+ * @since 31/7/2017
+ */
+public class RequestorTargetDTO {
+    private String requestor;
+    private String target;
+
+    public String getRequestor() {
+        return requestor;
+    }
+
+    public void setRequestor(String requestor) {
+        this.requestor = requestor;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+}
